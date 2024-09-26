a03a6a4c54ef841bae8e5126f1a8211102c9df00
Sashkisa <alexatanasov14@gmail.com>
2024-09-26 23:07:11 +0200

    BMI Calculator Done

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,26 +6,40 @@ let age = window.prompt("How old are you?");
 let ageElement = document.getElementById("age");
 
 let advice = document.getElementById("advice-text");
+let adviceCard = document.getElementById("advice-card");
+
 
 if (age < 18) {
+    adviceCard.className = "box has-background-grey-light";
     advice.textContent = `Sorry ${userName}, you are not old enough to get a proper advice from us`;
-    } else {
-    let height = window.prompt("How tall are you?");
+} else {
+    adviceCard.className = "box";
+
+    let height = window.prompt("Please enter your height (in m, ie. 1.87)");
     let heightElement = document.getElementById("height");
 
-    let weight = window.prompt("How much do you weight?");
+    let weight = window.prompt("Please enter your weight (in kg, ie. 85.5)");
     let weightElement = document.getElementById("weight");
+
+    let bmi = weight / (height * height);
 
     heightElement.textContent = height;
     weightElement.textContent = weight;
-    advice.innerHTML = `Your BMI: 28.3<br>You are slightly over weight<br>Start with cardio training`;
+    userNameElement.textContent = userName;
+    ageElement.textContent = age;
+
+    //Assigning a specific color and advice according to the BMI
+    if (bmi < 18.5) {
+        adviceCard.className = "box has-background-warning";
+        advice.innerHTML = `Your BMI: ${bmi}<br>You are under weight<br>Start with a personal trainer`;
+    } else if (bmi >= 18.5 && bmi < 25) {
+        adviceCard.className = "box has-background-success";
+        advice.innerHTML = `Your BMI: ${bmi}<br>You are a normal weight<br>Start with any programme`;
+    } else if (bmi >= 25 && bmi < 30) {
+        adviceCard.className = "box has-background-warning";
+        advice.innerHTML = `Your BMI: ${bmi}<br>You are slightly over weight<br>Start with cardio training`;
+    } else if (bmi >= 30) {
+        adviceCard.className = "box has-background-danger";
+        advice.innerHTML = `Your BMI: ${bmi}<br>You are obese<br>Start with a personal trainer`;
     }
-
-
-
-userNameElement.textContent = userName;
-ageElement.textContent = age;
-
-
-
-
+}
